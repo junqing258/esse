24c362fa1d5c47f09b8ccf2d4a5fd482adf5d720
Junqing <1551486328@qq.com>
2019-03-26 14:01:24 +0800

    tools packer

--- src/main/java/com/junqi/tools/TP.java
+++ src/main/java/com/junqi/tools/TP.java
@@ -2,8 +2,12 @@ package com.junqi.tools;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.io.IOException;
+import java.io.Writer;
 
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Page;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker.Rect;
 import com.badlogic.gdx.tools.texturepacker.TexturePacker.Settings;
 import com.badlogic.gdx.tools.texturepacker.TexturePackerFileProcessor;
 
@@ -14,7 +18,7 @@ public class TP {
 		Settings settings = new Settings();
 		settings.maxWidth = 512;
 		settings.maxHeight = 512;
-		MyTexturePacker.process(settings, "../images", "../game-android/assets", "game");
+		LayaTexturePacker.process(settings, "../images", "../game-android/assets", "game");
 
 //		new LwjglApplication(new Game(), "Game", 320, 480);
 		
@@ -28,9 +32,10 @@ public class TP {
 
 
 
-class MyTexturePacker extends TexturePacker {
+class LayaTexturePacker extends TexturePacker {
+	private Settings settings;
 	
-	public MyTexturePacker(File rootDir, Settings settings) {
+	public LayaTexturePacker(File rootDir, Settings settings) {
 		super(rootDir, settings);
 	}
 
@@ -49,5 +54,25 @@ class MyTexturePacker extends TexturePacker {
 		} catch (Exception ex) {
 			throw new RuntimeException("Error packing images.", ex);
 		}
+	}
+	
+	private void writeRect (Writer writer, Page page, Rect rect, String name) throws IOException {
+		writer.write(Rect.getAtlasName(name, settings.flattenPaths) + "\n");
+		writer.write("  rotate: " + rect.rotated + "\n");
+		writer
+			.write("  xy: " + (page.x + rect.x) + ", " + (page.y + page.height - rect.y - (rect.height - settings.paddingY)) + "\n");
+
+		writer.write("  size: " + rect.regionWidth + ", " + rect.regionHeight + "\n");
+		if (rect.splits != null) {
+			writer.write("  split: " //
+				+ rect.splits[0] + ", " + rect.splits[1] + ", " + rect.splits[2] + ", " + rect.splits[3] + "\n");
+		}
+		if (rect.pads != null) {
+			if (rect.splits == null) writer.write("  split: 0, 0, 0, 0\n");
+			writer.write("  pad: " + rect.pads[0] + ", " + rect.pads[1] + ", " + rect.pads[2] + ", " + rect.pads[3] + "\n");
+		}
+		writer.write("  orig: " + rect.originalWidth + ", " + rect.originalHeight + "\n");
+		writer.write("  offset: " + rect.offsetX + ", " + (rect.originalHeight - rect.regionHeight - rect.offsetY) + "\n");
+		writer.write("  index: " + rect.index + "\n");
 	}
 }
